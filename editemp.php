525e623d45a53d4c1556bf1327c589eb4fd6a135
fadidebow <34662511+fadidebow@users.noreply.github.com>
2021-02-09 00:35:24 +0200

    Update editemp.php

--- editemp.php
+++ editemp.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>Admin DashBoard</title>
+	
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" 
 	href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
